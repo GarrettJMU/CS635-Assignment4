29a8fc6036f02cab8684be614136a0e952763397
GarrettJMU <garretthughes3@gmail.com>
2019-12-08 17:05:04 -0800

    update more getting read

--- lib/run_array.rb
+++ lib/run_array.rb
@@ -6,16 +6,24 @@ class RunArray
   end
 
   def add_run(starting, ending, font)
-
+    font_array.push(build_object(starting, ending, font))
   end
 
   def append_run(ending, font)
-
+    font_array.push(build_object(get_last_index, ending, font))
   end
 
   private
 
-  def get_last_index
+  def build_object(starting, ending, font)
+    {
+      starting_index: starting,
+      ending_index: ending,
+      font: font
+    }
+  end
 
+  def get_last_index
+    font_array.present? ? self.last.starting_index : 0
   end
 end

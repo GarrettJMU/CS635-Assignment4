159414fa9844b91787f506c6206fd604002dc059
GarrettJMU <garretthughes3@gmail.com>
2019-12-09 20:10:36 -0800

    add specs

--- lib/font_flyweight.rb
+++ lib/font_flyweight.rb
@@ -1,7 +1,8 @@
+# require ''
 class FontFlyweight
   attr_reader :font_name, :font_size, :font_style
 
-  def initialize(font_name, font_size, font_style)
+  def initialize(font_name, font_size, font_style = nil)
     @font_name = font_name
     @font_size = font_size
     @font_style = font_style
---
+++ spec/font_flyweight_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require_relative '../lib/font_flyweight'
+
+RSpec.describe FontFlyweight do
+  describe '#initialize' do
+    it 'should set all instance variables correctly' do
+      subject = described_class.new('times new roman', 12, 'bold')
+      expect(subject.font_name).to eq('times new roman')
+      expect(subject.font_size).to eq(12)
+      expect(subject.font_style).to eq('bold')
+    end
+
+    it 'should default font style to nil' do
+      subject = described_class.new('times new roman', 12)
+
+      expect(subject.font_name).to eq('times new roman')
+      expect(subject.font_size).to eq(12)
+      expect(subject.font_style).to eq(nil)
+    end
+  end
+
+  describe '#get_font' do
+    it 'should return the font' do
+      subject = described_class.new('times new roman', 12, 'bold')
+      expect(subject.get_font).to eq({})
+    end
+  end
+end
